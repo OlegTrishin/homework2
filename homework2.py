53efb864d1be4bd8596be5dc7b172186f6bd685c
Oleg <otrishin@gmail.com>
2024-06-12 13:37:34 +1000

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+number_homework = 12                        #Количество выполненных ДЗ
+number_hours = 1.5                          #Количество затраченных часов
+name_course = "Python"                      #Название курса
+time_task = number_hours / number_homework  #Время на одно задание
+print("Курс:", name_course, ", всего задач:", number_homework, ", затрачено часов:", number_hours, ", среднее время выполнения", time_task, "часа." )
